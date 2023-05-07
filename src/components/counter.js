041a469d539427f4af39afaea86ae3a9b9afda72
forgitjo <forpcjo@gmail.com>
2023-05-07 12:41:37 +0900

    beginning_click_counter

---
+++ src/components/counter.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class Counter extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {count:0}
+    }
+
+    render(){
+        return(
+            <button></button>
+        )
+
+}
